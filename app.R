bb56dba1c796e429154684ef174d0a1073e67ed2
diss-git <134962322+diss-git@users.noreply.github.com>
2023-09-05 17:11:56 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -6,6 +6,12 @@ source("etl.R")
 
 # Define UI
 ui <- fluidPage(
+    
+    # Include your custom CSS file
+    includeCSS("www/styles.css"),
+    
+    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "https://www150.statcan.gc.ca/wet-boew4b/css/theme.min.css")),
+    
     # Add line under title for readability
     div(style = "border-bottom: 1px solid #333;",
         titlePanel("Population Analysis")),
@@ -21,22 +27,25 @@ ui <- fluidPage(
         # Add Chart Type dropdown (Population or Growth Rate)
         column(width = 6,
                selectInput(inputId = "chart_type_toggle", label = "Value",
-                           choices = c("Population", "Growth Rate"),
+                           choices = c("Population", "Growth rate"),
                            selected = "Population", multiple = FALSE))
     ),
     
-    # Add CSS style for rounded borders
-    tags$style(HTML("
-    .rounded-border {
-      border-radius: 5px;
-    }
-  ")),
+    # Skip link for accessibility
+    tags$div(
+            tags$a(href = "#formatted_table", class = "wb-inv wb-show-onfocus wb-sl",
+                    "Skip the interactive chart and go directly to the data table")
+    ),
   
   # Render Highchart
   highchartOutput("line_chart"),
   
+  # Add line break between graph and table
+  tags$br(), 
+  
   # Render formatted DataTable
-  DTOutput("formatted_table")
+    DTOutput("formatted_table")
+  
 )
 
 # Define server
@@ -53,34 +62,36 @@ server <- function(input, output) {
             # Render Highchart for Population
             highchart() %>%
                 hc_chart(type = "line") %>%
-                hc_title(text = "Population over time") %>%
-                hc_xAxis(categories = filtered_data()$Year) %>%
-                hc_yAxis(title = list(text = "Population"),
+                hc_title(text = "<b>Population over time</b>",
+                         align = "left") %>%
+                hc_xAxis(categories = filtered_data()$"Reference period") %>%
+                hc_yAxis(title = list(text = "population"),
                          labels = list(
                              formatter = JS("function() {
                                  return Highcharts.numberFormat(this.value, 0, '.', ',');
                              }")
                          )) %>%
-                hc_add_series(name = "Population", data = filtered_data()$Population, color = "navy") %>%
+                hc_add_series(name = "Population", data = filtered_data()$Population, color = "navy",showInLegend = F) %>%
                 hc_tooltip(pointFormatter = JS("
                     function() {
                         var formattedValue = Highcharts.numberFormat(this.y, 0, '.', ',');
                         return '<span style=\"color:' + this.series.color + '\">' + this.series.name + '</span>: <b>' + formattedValue + '</b><br/>';
                     }
                 "))
-        } else if (input$chart_type_toggle == "Growth Rate") {
+        } else if (input$chart_type_toggle == "Growth rate") {
             # Render Highchart for Growth Rate
             highchart() %>%
                 hc_chart(type = "line") %>%
-                hc_title(text = "Growth rate over time") %>%
-                hc_xAxis(categories = filtered_data()$Year) %>%
-                hc_yAxis(title = list(text = "Growth rate (per 1000)"),
+                hc_title(text = "<b>Growth rate over time percentage</b>",
+                         align = "left") %>%
+                hc_xAxis(categories = filtered_data()$"Reference period") %>%
+                hc_yAxis(title = list(text = "percentage"),
                          labels = list(
                              formatter = JS("function() {
                                  return Highcharts.numberFormat(this.value, 0, '.', ',') + '%';
                              }")
                          )) %>%
-                hc_add_series(name = "Growth Rate", data = filtered_data()$"Growth Rate", color = "green") %>%
+                hc_add_series(name = "Growth rate", data = filtered_data()$"Growth rate", color = "green", showInLegend = F) %>%
                 hc_tooltip(pointFormatter = JS("
                     function() {
                         var formattedValue = Highcharts.numberFormat(this.y, 2, '.', ',') + '%';
@@ -94,18 +105,17 @@ server <- function(input, output) {
     output$formatted_table <- renderDT({
         datatable(filtered_data(), 
                   options = list(pageLength = 10), 
-                  class = 'cell-border stripe rounded-border',
                   rownames = FALSE) %>%
-            formatStyle(columns = c("Year", "Geography", "Population", "Growth Rate"),
+            formatStyle(columns = c("Reference period", "Geography", "Population", "Growth rate"),
                         backgroundColor = 'white',
                         textAlign = 'left') %>%
             formatCurrency("Population", currency = "", interval = 3, mark = ",") %>%
-            formatRound("Growth Rate", digits = 2) %>%
+            formatRound("Growth rate", digits = 2) %>%
             formatRound("Population", digits = 0) %>%
             formatStyle("Population", textAlign = "right") %>%
-            formatStyle("Growth Rate", textAlign = "left") %>%
+            formatStyle("Growth rate", textAlign = "left") %>%
             # Align column name to the right
-            formatStyle(names(filtered_data())[names(filtered_data()) == "Growth Rate"],
+            formatStyle(names(filtered_data())[names(filtered_data()) == "Growth rate"],
                         textAlign = "right")  
     })
     
